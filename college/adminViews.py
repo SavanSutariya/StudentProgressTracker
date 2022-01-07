45c55cda3c465c56d47313835ba80cf81b22c4d8
Savan Sutariya <savanpatel5248@gmail.com>
2022-01-07 16:46:45 +0530

    course table and routes

--- college/adminViews.py
+++ college/adminViews.py
@@ -1,4 +1,12 @@
 from django.shortcuts import redirect,render
-
+from .models import *
 def Home(request):
     return render(request,"college/college_dashboard.html")
+def Course_list(request):
+    courses = Cource.objects.filter(college=request.user.college)
+    return render(request,"college/courses_list.html",{'courses':courses})
+def Course_details(request,pk):
+    
+    subjects_list = Subject.objects.filter(semester=pk)
+    print(subjects_list)
+    return render(request, "college/course_details.html")
--- college/urls.py
+++ college/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path
 from .adminViews import *
 urlpatterns = [
-    path("",Home, name="college-home"),
+    # college-admin urls
+    path('',Home, name="college-home"),
+    path('course/<int:pk>/',Course_details, name="college-course-detail"),
+    path('courses/',Course_list, name="college-course-list"),
 ]
