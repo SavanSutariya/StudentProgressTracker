94948244167b8f865615c639dd495e478f50c805
Pratham Doshi <parthamdoshi25@gmail.com>
2022-01-10 12:40:07 +0530

    Add subject Fronted / Backed

--- college/adminViews.py
+++ college/adminViews.py
@@ -1,4 +1,4 @@
-from django.shortcuts import redirect,render
+from django.shortcuts import HttpResponse,redirect,render
 from .models import *
 def Home(request):
     return render(request,"college/college_dashboard.html")
@@ -9,6 +9,7 @@ def Course_list(request):
 def subjects_list(request,pk):
     subjects_list = []
     semesters_list = Semester.objects.filter(course=pk)
+
     for semester in semesters_list:
         subjects = Subject.objects.filter(semester=semester)
         if subjects.exists():
@@ -18,3 +19,37 @@ def subjects_list(request,pk):
         'subjects_list':subjects_list,
     }
     return render(request, "college/subjects_list.html",context)
+
+def add_subject(request,pk):
+    semesters_list = Semester.objects.filter(course=pk)
+    subject_type = SubjectType.objects.all()
+
+
+    if request.method == "POST":
+        subject_code =  request.POST.get('subject_code')
+        subject_name =  request.POST.get('subject_name')
+        sub_type =  SubjectType.objects.get(id = request.POST.get('subject_type'))
+        semesters =  Semester.objects.get(id = request.POST.get('semesters_list'))
+        
+
+        '''if Subject.objects.filter(code=subject_code).exists():
+            print('Alredy added')
+            return redirect('add-subject',pk)
+        else:                subject = Subject(
+                code = subject_code,
+                name = subject_name,
+                sub_type = sub_type,
+                semester = semesters
+            )
+            subject.save()
+            print('Added successfully')
+            return redirect('add-student',pk)'''
+        
+        print(subject_code,subject_name,sub_type,semesters)
+ 
+    context = {
+        'subject_type' : subject_type,
+        'semesters_list' : semesters_list
+    }
+    
+    return render(request, "college/add_subject.html",context)
--- college/urls.py
+++ college/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from .adminViews import *
+
 urlpatterns = [
     # college-admin urls
     path('' ,Home, name="college-home"),
     path('course/<int:pk>/',subjects_list, name="subjects-list"),
     path('courses/',Course_list, name="college-course-list"),
+    path('course/addsubject/<int:pk>',add_subject,name="add-subject")
 ]
