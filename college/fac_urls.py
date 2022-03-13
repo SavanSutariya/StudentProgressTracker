70b5760a7ff08eecba004a01b5f382566563a02c
Pratham Doshi <parthamdoshi25@gmail.com>
2022-03-13 11:57:46 +0530

    student done

--- college/fac_urls.py
+++ college/fac_urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import facultyViews
 urlpatterns = [
     path('', facultyViews.Home, name="faculty-home"),
-    path('profile/',facultyViews.user_profile, name="faculty-profile"),
+    #path('profile/',facultyViews.user_profile, name="faculty-profile"),
     path('papers/',facultyViews.papers_list,name="faculty-papers-list"),
     path('papers/<int:pk>/',facultyViews.paper_marks,name="faculty-paper-marks"),
 
--- college/stu_urls.py
+++ college/stu_urls.py
@@ -1,8 +1,13 @@
-from django.urls import URLPattern, path
+from django.urls import path
 from . import studentViews
 
 urlpatterns = [
-    path('', studentViews.Home, name="student-home"),
+    path('' ,studentViews.Home, name="student-home"),
+    path('profile/',studentViews.user_profile,name="college-student-profile"),
+    path('checkResult/',studentViews.check_result,name="college-student-result"),
+    path('ajax/get-papers/<int:pk>',view=studentViews.get_papers_ajax, name="ajax-get-no-of-papers"),
+     path('ajax/get-marks/<int:pk>',view=studentViews.get_marks_ajax, name="ajax-get-no-of-marks"),
+    
 ]
 
 
