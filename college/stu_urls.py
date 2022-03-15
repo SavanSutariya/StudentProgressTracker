b83fa90a0f98ae3ea99a22ec97d1792980ce65ce
Savan Sutariya <savanpatel5248@gmail.com>
2022-03-15 11:24:29 +0530

    display student marks and js file created

--- college/stu_urls.py
+++ college/stu_urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
     path('profile/',studentViews.user_profile,name="college-student-profile"),
     path('checkResult/',studentViews.check_result,name="college-student-result"),
     path('ajax/get-papers/<int:pk>',view=studentViews.get_papers_ajax, name="ajax-get-no-of-papers"),
-     path('ajax/get-marks/<int:pk>',view=studentViews.get_marks_ajax, name="ajax-get-no-of-marks"),
+    path('ajax/get-marks/<int:pk>',view=studentViews.get_marks_ajax, name="ajax-get-no-of-marks"),
     
 ]
 
