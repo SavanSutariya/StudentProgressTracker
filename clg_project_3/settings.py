9bb65f15b1c74167e4bbfed660382b7c6af80e45
Savan Sutariya <savanpatel5248@gmail.com>
2022-04-05 00:35:50 +0530

    Heroku

--- clg_project_3/settings.py
+++ clg_project_3/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
+import django_heroku
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -24,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-ow!8uodx%76h_0t_42e__!p0%0l$=us(gphegd1%o(q0z!i76i'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
@@ -133,3 +135,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 AUTH_USER_MODEL = 'college.CustomUser'
 
 ACTIVE_LINK_STRICT = True
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+django_heroku.settings(locals())
