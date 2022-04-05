d70abbad40a38698b797aa2f0622fc92a5449afc
Savan Sutariya <savanpatel5248@gmail.com>
2022-04-05 12:15:07 +0530

    data dump

--- college/models.py
+++ college/models.py
@@ -29,7 +29,7 @@ class CustomUser(AbstractUser):
     USER = (('1',"CollegeAdmin"),('2',"Faculty"),('3',"Student") )
     userType = models.CharField(choices=USER,default=1, max_length=50)
     profile_pic = models.ImageField(upload_to='media/profile_pic')
-    college = models.ForeignKey(College, on_delete=models.CASCADE)
+    college = models.ForeignKey(College, on_delete=models.CASCADE, null=True)
 
 class Faculty(models.Model):
     user = models.OneToOneField(CustomUser , on_delete=models.CASCADE)
